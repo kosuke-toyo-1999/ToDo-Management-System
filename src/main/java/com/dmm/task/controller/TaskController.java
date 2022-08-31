daf0929da7be447f297be1a74c22415582beaf57
kosuke toyo <107592609+kosuke-toyo-1999@users.noreply.github.com>
2022-09-01 00:18:47 +0900

    更新

--- src/main/java/com/dmm/task/controller/TaskController.java
+++ src/main/java/com/dmm/task/controller/TaskController.java
@@ -21,8 +21,6 @@ import com.dmm.task.entity.Tasks;
 import com.dmm.task.repository.TasksRepository;
 import com.dmm.task.service.AccountUserDetails;
 
-import ch.qos.logback.core.util.Duration;
-
 @Controller
 public class TaskController {
 
@@ -40,35 +38,40 @@ public class TaskController {
 
 		// ③ その月の1日のLocalDateを取得する
 		LocalDate day = LocalDate.now();
-
+		day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);  
 
 		// ④ 曜日を表すDayOfWeekを取得し
-		day.getYear();
-		day.getMonthValue();
-		day.getDayOfWeek();
-		
-		day(LocalDate.minusDays(DayOfWeek.getValue()));
+		DayOfWeek DayOfWeek = day.getDayOfWeek();
+
 
 		// ④ 上で取得したLocalDateに曜日の値（DayOfWeek#getValue)をマイナスして前月分のLocalDateを求める
 		// https://qiita.com/tora_kouno/items/d230f904a2b768ccb319
 		// http://gucci1208.com/2%E3%81%A4%E3%81%AEcalendar%E3%82%AF%E3%83%A9%E3%82%B9%E3%82%92%E6%AF%94%E8%BC%83%E3%81%97%E3%81%A6%E3%80%81%E6%97%A5%E6%95%B0%E5%B7%AE%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B-488.html
 		// http://heppoen.seesaa.net/article/480642483.html
-		Duration lastDay = Duration.between(startDay.getvlaue - calendar.getvlaue);
+		day = day.minusDays(DayOfWeek.getValue());
 
 		// ⑤ 1日ずつ増やしてLocalDateを求めていき、2．で作成したListへ格納していき、1週間分詰めたら1．のリストへ格納する
 
 		for (int k = 0; k >= 1; k++) {
+		
 			for (int i = 0; i >= 7; i++) {
-				week.add(Calendar.DATE, 1);
+				
+				day.plusDays(1);
+			
 			}
+			
 			matrix.add(week);
+		
 		}
 
 		// ⑥2週目以降は単純に1日ずつ日を増やしながらLocalDateを求めてListへ格納していき、土曜日になったら1．のリストへ格納して新しいListを生成する（月末を求めるにはLocalDate#lengthOfMonth()を使う）
 
-		for (int m = 0; m == Calendar.THURSDAY; m++) {
+		for (int m = 0; m >= 7; m++) {
+			
 			for (int i = 0; i >= 7; i++) {
-				week.add(Calendar.DATE, 1);
+				
+				day.plusDays(1);
+				
 			}
 			
 			matrix.add(week);
@@ -77,8 +80,11 @@ public class TaskController {
 		// ⑦ 最終週の翌月分をDayOfWeekの値を使って計算し、
 		
 		for (int n = 0; n == Calendar.lengthOfMonth; n++) {
+			
 			for (int i = 0; i >= 7; i++) {
+				
 				week.add(Calendar.DATE, 1);
+				
 			}
 			
 			matrix.add(week);
